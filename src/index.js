3a71f8ca2358a070258af6c36185a447b66a3ee3
japuammelo <dimeloarquitetura@gmail.com>
2022-03-15 17:18:36 -0300

    Feat - Commit Inicial
    + Add: Criando arquivos de rota, index e config do banco.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const db = require('./config/db')
+const routes = require('./routes')
+require('dotenv').config()
+
+const app = express()
+db.connect()
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use('/api', routes)
+
+app.all('*', (req, res, next) => {
+    return res.status(404).json({
+        erro: {
+            message: 'Rota não encontrada.',
+        },
+    })
+})
+
+app.use((erro, req, res, next) => {
+    return res.status(erro.status || 500).json({
+        erro: {
+            message: 'Erro interno do Servidor.',
+        },
+    })
+})
+

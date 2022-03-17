cede8c6039861aab94fb2eeb82cfd6060997f97e
japuamelo <dimeloarquitetura@gmail.com>
2022-03-17 09:16:39 -0300

    Adicionando método de auto-increment

--- src/models/patient.js
+++ src/models/patient.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const patientSchema = new mongoose.Schema({
     _id: {
-        type: ObjectId,
+        type: Schema.Types.ObjectId,
         defaultValue: true,
-        primaryKey: key,
         unique: [true]
-    }
+    },
+
 })
 
 module.exports = mongoose.model('patient', patientSchema)

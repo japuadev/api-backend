87f2d47f796d7d7406044e9141a235e219437088
japuamelo <dimeloarquitetura@gmail.com>
2022-03-17 15:43:20 -0300

    Feat - Controllers Patient e Program.
    + Add: Adicionando arquivos de controllers de pacientes e programas e suas respectivas rotas.

---
+++ src/controller/ProgramController.js
@@ -0,0 +1,88 @@
+const Program = require('../models/program')
+
+module.exports = {
+    async create(req, res) {
+        try {
+            const body = req.body
+            const program = await Program.create(body)
+            return res.status(201).json(program)
+        } catch (error) {
+            return res.status(500).json({
+                error: {
+                    message: 'Erro ao criar Programa.',
+                    error: error.message,
+                },
+            })
+        }
+    },
+    async update(req, res) {
+        try {
+            const {id} = req.params
+            const body = req.body
+            const program = await Program.findByIdAndUpdate(id, body, {new: true})
+            if (program) {
+                return res.status(200).json(program)
+            } else {
+                return res.status(404).json({
+                    error: {
+                        message: 'Programa não encontrado.',
+                        error: `Não foi possivel encontrar o Programa com o id ${id}.`,
+                    },
+                })
+            }
+        } catch (error) {
+            return res.status(500).json({
+                error: {
+                    message: 'Erro ao atualizar Programa.',
+                    error: error.message,
+                },
+            })
+        }
+    },
+    async delete(req, res) {
+        try {
+            const {id} = req.params
+            const program = await Program.findByIdAndDelete(id)
+            if (program) {
+                return res.status(204).json({})
+            } else {
+                return res.status(404).json({
+                    error: {
+                        message: 'Programa não encontrado.',
+                        error: `Não foi possivel encontrar o Programa com o id ${id}.`,
+                    },
+                })
+            }
+        } catch (error) {
+            return res.status(500).json({
+                error: {
+                    message: 'Erro ao deletar o Programa.',
+                    error: error.message,
+                },
+            })
+        }
+    },
+    async getById(req, res) {
+        try {
+            const {id} = req.params
+            const program = await Program.findById(id)
+            if (program) {
+                return res.status(200).json(program)
+            } else {
+                return res.status(404).json({
+                    error: {
+                        messege: 'Programa não existe',
+                        error: `Não foi possivel encontrar o Programa com o id ${id}`,
+                    },
+                })
+            }
+        } catch (error) {
+            return res.status(500).json({
+                error: {
+                    message: 'Erro ao deletar o Programa.',
+                    error: error.message,
+                },
+            })
+        }
+    },
+}
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,19 @@
 const {Router} = require('express')
+
+const PatientController = require('./controller/PatientController')
+const ProgramController = require('./controller/ProgramController')
 const routes = Router()
+
+//Rotas de Paciente
+routes.post('/patients', PatientController.create)
+routes.get('/patients/:id', PatientController.getById)
+routes.put('/patients/:id', PatientController.update)
+routes.delete('/patients/:id', PatientController.delete)
+
+//Rotas de Programa
+routes.post('/programs', ProgramController.create)
+routes.get('/programs/:id', ProgramController.getById)
+routes.put('/programs/:id', ProgramController.update)
+routes.delete('/programs/:id', ProgramController.delete)
 
 module.exports = routes
